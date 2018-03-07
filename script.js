4af6cef8ebc5df93dbc3b0fe7cd40ca5966547a7
Juan Gesino <juangesino@gmail.com>
2018-03-07 07:10:19 +0100

    Add noopener to links 🔒

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var links = document.getElementsByClassName('storylink');
 for (var i = 0; i < links.length; i++) {
   links[i].setAttribute('target', '_blank');
+  links[i].setAttribute('rel', 'noopener');
 }
 console.log("HackerNews Links Extension: Links will now open in a new tab.");
